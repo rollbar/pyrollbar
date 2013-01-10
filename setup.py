aee3457003e14a860ccefd6e841e143222b01bac
sbezboro <sergei.b.ru@gmail.com>
2013-01-09 19:38:52 -0800

    up version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except IOError:
 setup(
     name='ratchet',
     packages=['ratchet'],
-    version='0.2.1',
+    version='0.3.0',
     description='Ratchet.io generic python library',
     long_description=README,
     author='Brian Rue',
