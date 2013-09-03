bd1daabd5df3eb269886373e164d5fd33879cbf9
Alex Laties <alex@voxy.com>
2013-09-03 16:08:18 -0400

    update plugin to use pyrollbar support for bottle requests

--- rollbar/contrib/bottle/__init__.py
+++ rollbar/contrib/bottle/__init__.py
@@ -1,24 +1,5 @@
 import bottle, rollbar, sys
 
-to_collect = (
-    'body',
-    'COOKIES',
-    'content_type',
-    'content_length',
-    'headers',
-    'method',
-    'path',
-    'remote_addr',
-    'remote_route',
-)
-methods = {}
-for tc in to_collect:
-    methods[tc] = lambda x: x
-
-methods['body'] = lambda x: u''.join(x)
-methods['COOKIES'] = lambda x: u'\n'.join([u'%s: %s' % (k, v) for k, v in x.items()])
-methods['headers'] = lambda x: u'\n'.join([u'%s: %s' % (k, v) for k, v in x.items()])
-
 class RollbarBottleReporter(object):
     '''
     A Bottle plugin that reports errors to Rollbar
@@ -35,11 +16,7 @@ class RollbarBottleReporter(object):
             try:
                 return callback(*args, **kwargs)
             except Exception, e:
-                info = {}
-                for k in to_collect:
-                    info[k] = methods[k](getattr(bottle.request, k, None))
-                info['error'] = u'%s' % e
-                rollbar.report_exc_info(sys.exc_info(), extra_data=info)
+                rollbar.report_exc_info(sys.exc_info(), request=bottle.request)
                 raise
 
         return wrapper
