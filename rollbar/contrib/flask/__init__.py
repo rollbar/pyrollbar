7f2fec789d66d61d8f457e33a7cf7f8b016fa4cb
Cory Virok <coryvirok@gmail.com>
2014-01-09 13:51:17 -0800

    report flask context @brianr

--- rollbar/contrib/flask/__init__.py
+++ rollbar/contrib/flask/__init__.py
@@ -12,5 +12,9 @@ def report_exception(app, exception):
     payload_data = {
         'framework': 'Flask',
     }
+
+    if request.url_rule:
+        payload_data['context'] = str(request.url_rule)
+
     rollbar.report_exc_info(sys.exc_info(), request, payload_data=payload_data)
     
