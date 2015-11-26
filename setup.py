d4b4bf5854f1f896949f99a33068d5ea29e86699
Brian Rue <brian@rollbar.com>
2015-11-25 19:18:16 -0800

    add python 3.5 version classifier

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
