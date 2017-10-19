da2b735eaeb004d42f32f1f1709af0b558f6dc8f
Andrew Weiss <wvvwwvw@gmail.com>
2017-10-19 09:54:56 -0700

    attempt to fix pypi rst readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
-README_PATH = os.path.join(HERE, 'README')
+README_PATH = os.path.join(HERE, 'README.rst')
 try:
     with open(README_PATH) as fd:
         README = fd.read()
