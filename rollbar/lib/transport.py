b9b899b747ef62eafdf45400f77f8143dd5aea73
Andrew Weiss <wvvwwvw@gmail.com>
2018-06-12 15:14:15 -0700

    requests.Session is not thread-safe, use thread local storage

--- rollbar/lib/transport.py
+++ rollbar/lib/transport.py
@@ -1,14 +1,23 @@
 import requests
+import threading
 
-_session = requests.Session()
+
+_local = threading.local()
+
+
+def _session():
+    if hasattr(_local, 'session'):
+        return _local.session
+    _local.session = requests.Session()
+    return _local.session
 
 
 def post(*args, **kw):
-    return _session.post(*args, **kw)
+    return _session().post(*args, **kw)
 
 
 def get(*args, **kw):
-    return _session.get(*args, **kw)
+    return _session().get(*args, **kw)
 
 
 __all__ = ['post', 'get']
