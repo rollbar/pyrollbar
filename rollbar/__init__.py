d37d68d20fcffe97ea3cb131216b205a0883271b
Aren Sandersen <aren@users.noreply.github.com>
2018-12-18 13:22:19 -0800

    Treq now requires bytes, not str, so encoding is up to the client (#298)
    * Treq now requires bytes, not str, so encoding is up to the client
    
    * Update __init__.py

--- rollbar/__init__.py
+++ rollbar/__init__.py
@@ -1455,12 +1455,16 @@ def _post_api_twisted(path, payload_str, access_token=None):
         r.headers.update(resp.headers.getAllRawHeaders())
         return treq.content(resp).addCallback(post_data_cb, r)
 
-    headers = {'Content-Type': ['application/json']}
+    headers = {'Content-Type': ['application/json; charset=utf-8']}
     if access_token is not None:
         headers['X-Rollbar-Access-Token'] = [access_token]
 
     url = urljoin(SETTINGS['endpoint'], path)
-    d = treq.post(url, payload_str, headers=headers,
+    try:
+        encoded_payload = payload_str.encode('utf8')
+    except (UnicodeDecodeError, UnicodeEncodeError):
+        encoded_payload = payload_str
+    d = treq.post(url, encoded_payload, headers=headers,
                   timeout=SETTINGS.get('timeout', DEFAULT_TIMEOUT))
     d.addCallback(post_cb)
 
