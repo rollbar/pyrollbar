122e7e0bc9aceb218920f0598716d9da64b5b508
Tómas Árni Jónasson <tomas@jyve.com>
2019-08-30 00:06:37 -0700

    Handle recursive re-raising of exceptions

--- rollbar/test/test_rollbar.py
+++ rollbar/test/test_rollbar.py
@@ -281,6 +281,52 @@ class RollbarTest(BaseTest):
         self.assertEqual(payload['data']['body']['trace_chain'][1]['frames'][-1]['locals']['bar_local'], 'bar')
 
     @mock.patch('rollbar.send_payload')
+    def test_report_exception_with_same_exception_as_cause(self, send_payload):
+        def _raise_cause():
+            bar_local = 'bar'
+            raise CauseException('bar')
+
+        def _raise_ex():
+            try:
+                _raise_cause()
+            except CauseException as cause:
+                # python2 won't automatically assign this traceback...
+                exc_info = sys.exc_info()
+                setattr(cause, '__traceback__', exc_info[2])
+
+                try:
+                    foo_local = 'foo'
+                    # in python3 this would normally be expressed as
+                    # raise cause from cause
+                    setattr(cause, '__cause__', cause)  # PEP-3134
+                    raise cause
+                except:
+                    rollbar.report_exc_info()
+
+        _raise_ex()
+
+        self.assertEqual(send_payload.called, True)
+
+        payload = send_payload.call_args[0][0]
+
+        self.assertEqual(payload['access_token'], _test_access_token)
+        self.assertIn('body', payload['data'])
+        self.assertNotIn('trace', payload['data']['body'])
+        self.assertIn('trace_chain', payload['data']['body'])
+        self.assertEqual(2, len(payload['data']['body']['trace_chain']))
+
+        self.assertIn('exception', payload['data']['body']['trace_chain'][0])
+        self.assertEqual(payload['data']['body']['trace_chain'][0]['exception']['message'], 'bar')
+        self.assertEqual(payload['data']['body']['trace_chain'][0]['exception']['class'], 'CauseException')
+        frames = payload['data']['body']['trace_chain'][0]['frames']
+        self.assertEqual(payload['data']['body']['trace_chain'][0]['frames'][0]['locals']['foo_local'], 'foo')
+
+        self.assertIn('exception', payload['data']['body']['trace_chain'][1])
+        self.assertEqual(payload['data']['body']['trace_chain'][1]['exception']['message'], 'bar')
+        self.assertEqual(payload['data']['body']['trace_chain'][1]['exception']['class'], 'CauseException')
+        self.assertEqual(payload['data']['body']['trace_chain'][1]['frames'][-1]['locals']['bar_local'], 'bar')
+
+    @mock.patch('rollbar.send_payload')
     def test_report_exception_with_context(self, send_payload):
         def _raise_context():
             bar_local = 'bar'
