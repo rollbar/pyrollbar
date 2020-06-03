cdfc35b32af7b83d2cc65d3837ff707828d1c965
pgajdos <pgajdos@suse.cz>
2020-06-03 11:01:26 +0200

    fallback to unittest when unittest2 is not available; unittest2 is not neccessary in python3

--- rollbar/test/__init__.py
+++ rollbar/test/__init__.py
@@ -1,9 +1,12 @@
-import unittest2
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
 
 
 SNOWMAN = b'\xe2\x98\x83'
 SNOWMAN_UNICODE = SNOWMAN.decode('utf8')
 
 
-class BaseTest(unittest2.TestCase):
+class BaseTest(unittest.TestCase):
     pass
