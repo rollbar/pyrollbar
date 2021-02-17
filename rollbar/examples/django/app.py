7033c15599a098aee9591b08cb776751e58315e0
Bart Skowron <bart.skowron@rollbar.com>
2021-02-17 19:10:54 +0100

    Refactor redundant settings

--- rollbar/examples/django/app.py
+++ rollbar/examples/django/app.py
@@ -8,45 +8,38 @@ from django.conf import settings
 # use settings compatible with the installed Django version
 # v1.10+ requires MIDDLEWARE keyname
 # older versions require MIDDLEWARE_CLASSES keyname
+
+ROLLBAR_CONFIG = {
+    'access_token': 'POST_SERVER_ITEM_ACCESS_TOKEN',
+    'environment': 'development',
+    'branch': 'master',
+    'root': os.getcwd()
+}
+
+MIDDLEWARE_CONFIG = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Rollbar middleware
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
+)
+
 if django.VERSION >= (1, 10):
     settings.configure(
         DEBUG=True,
         SECRET_KEY='thisisthesecretkey',
         ROOT_URLCONF=__name__,
-        ROLLBAR = {
-            'access_token': 'POST_SERVER_ITEM_ACCESS_TOKEN',
-            'environment': 'development',
-            'branch': 'master',
-            'root': os.getcwd()
-        },
-        MIDDLEWARE = (
-            'django.middleware.common.CommonMiddleware',
-            'django.middleware.csrf.CsrfViewMiddleware',
-            'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-            # Rollbar middleware
-            'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
-        ),
+        ROLLBAR = ROLLBAR_CONFIG,
+        MIDDLEWARE = MIDDLEWARE_CONFIG,
     )
 else:
     settings.configure(
         DEBUG=True,
         SECRET_KEY='thisisthesecretkey',
         ROOT_URLCONF=__name__,
-        ROLLBAR = {
-            'access_token': 'POST_SERVER_ITEM_ACCESS_TOKEN',
-            'environment': 'development',
-            'branch': 'master',
-            'root': os.getcwd()
-        },
-        MIDDLEWARE_CLASSES = (
-            'django.middleware.common.CommonMiddleware',
-            'django.middleware.csrf.CsrfViewMiddleware',
-            'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-            # Rollbar middleware
-            'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
-        ),
+        ROLLBAR = ROLLBAR_CONFIG,
+        MIDDLEWARE_CLASSES = MIDDLEWARE_CONFIG,
     )
 
 from django.conf.urls import url
