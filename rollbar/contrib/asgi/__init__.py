5f5530205b54ca7929376c3c8e8d2c1fd68378a4
Bart Skowron <bxsx@bartskowron.com>
2021-03-18 22:50:21 +0100

    Add typing support for Starlette-based apps

--- rollbar/contrib/asgi/__init__.py
+++ rollbar/contrib/asgi/__init__.py
@@ -1,16 +1,35 @@
 import rollbar
 
+try:
+    from starlette.types import ASGIApp, Scope, Receive, Send
+except ImportError:
+    STARLETTE_INSTALLED = False
+else:
+    STARLETTE_INSTALLED = True
 
-class ASGIMiddleware:
-    def __init__(self, app):
-        self.app = app
 
-    async def __call__(self, scope, receive, send):
-        try:
-            await self.app(scope, receive, send)
-        except Exception:
-            rollbar.report_exc_info()
-            raise
+if STARLETTE_INSTALLED is True:
+    class ASGIMiddleware:
+        def __init__(self, app: ASGIApp) -> None:
+            self.app = app
+
+        async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+            try:
+                await self.app(scope, receive, send)
+            except Exception:
+                rollbar.report_exc_info()
+                raise
+else:
+    class ASGIMiddleware:
+        def __init__(self, app):
+            self.app = app
+
+        async def __call__(self, scope, receive, send):
+            try:
+                await self.app(scope, receive, send)
+            except Exception:
+                rollbar.report_exc_info()
+                raise
 
 
 def _hook(request, data):
