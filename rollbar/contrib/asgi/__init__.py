3d5a70074ccc4efce6a9de197f568f9580e2c84f
Bart Skowron <bxsx@bartskowron.com>
2021-03-19 21:40:46 +0100

    Add support for type hints if Starlette package installed
    This feature adds support for type hints in popular IDEs such as PyCharm or
    Visual Studio Code.
    
    The Python IDEs cannot infer type hinting for arbitrary dynamic code.
    So, annotations must be explicitly declared in the class body. To make
    Starlette installation optional, the class definition must be
    duplicated.

--- rollbar/contrib/asgi/__init__.py
+++ rollbar/contrib/asgi/__init__.py
@@ -8,32 +8,40 @@ else:
     STARLETTE_INSTALLED = True
 
 
+# Optional class annotations must be statically declared because
+# IDEs cannot infer type hinting for arbitrary dynamic code
+def ASGIApp(cls):
+    async def asgi_app(self, scope, receive, send):
+        try:
+            await self.app(scope, receive, send)
+        except Exception:
+            rollbar.report_exc_info()
+            raise
+
+    cls.asgi_app = asgi_app
+    return cls
+
+
 if STARLETTE_INSTALLED is True:
 
+    @ASGIApp
     class ASGIMiddleware:
         def __init__(self, app: ASGIApp) -> None:
             self.app = app
 
         async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-            try:
-                await self.app(scope, receive, send)
-            except Exception:
-                rollbar.report_exc_info()
-                raise
+            await self.asgi_app(scope, receive, send)
 
 
 else:
 
+    @ASGIApp
     class ASGIMiddleware:
         def __init__(self, app):
             self.app = app
 
         async def __call__(self, scope, receive, send):
-            try:
-                await self.app(scope, receive, send)
-            except Exception:
-                rollbar.report_exc_info()
-                raise
+            await self.asgi_app(scope, receive, send)
 
 
 def _hook(request, data):
--- rollbar/test/asgi_tests/test_asgi.py
+++ rollbar/test/asgi_tests/test_asgi.py
@@ -14,3 +14,16 @@ class ASGIMiddlewareTest(BaseTest):
         import rollbar.contrib.asgi
 
         self.assertEqual(rollbar.BASE_DATA_HOOK, rollbar.contrib.asgi._hook)
+
+    def test_should_support_type_hints_if_starlette_installed(self):
+        try:
+            from starlette.types import ASGIApp, Receive, Scope, Send
+        except ImportError:
+            self.skipTest("Support for type hints requires Starlette to be installed")
+
+        import rollbar.contrib.asgi
+
+        self.assertDictEqual(
+            rollbar.contrib.asgi.ASGIMiddleware.__call__.__annotations__,
+            {"scope": Scope, "receive": Receive, "send": Send, "return": None},
+        )
