4965ebdefdad3df819315dce078e2c2f12fa7eb5
Bart Skowron <bart.skowron@rollbar.com>
2021-04-23 01:13:04 +0200

    Move StarletteMiddleware to separate module

--- rollbar/contrib/starlette/__init__.py
+++ rollbar/contrib/starlette/__init__.py
@@ -1,31 +1,9 @@
 __all__ = ['StarletteMiddleware']
 
-import sys
-
 from starlette import __version__
-from starlette.requests import Request
-from starlette.types import Receive, Scope, Send
 
 import rollbar
-from rollbar.contrib.asgi import ASGIMiddleware
-
-
-class StarletteMiddleware(ASGIMiddleware):
-    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        try:
-            await self.app(scope, receive, send)
-        except Exception:
-            if scope['type'] == 'http':
-                request = Request(scope, receive, send)
-
-                # Consuming the request body in Starlette middleware is problematic
-                # See: https://github.com/encode/starlette/issues/495#issuecomment-494008175
-                # Uncomment line below if you know the risk
-                # await request.body()
-
-                exc_info = sys.exc_info()
-                rollbar.report_exc_info(exc_info, request)
-            raise
+from rollbar.contrib.starlette.middleware import StarletteMiddleware
 
 
 def _hook(request, data):
