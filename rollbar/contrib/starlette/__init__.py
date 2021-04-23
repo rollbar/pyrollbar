d5785793af10925b2b5f78b24ea923daf5208e80
Bart Skowron <bart.skowron@rollbar.com>
2021-04-23 04:29:17 +0200

    Export get_current_request()
    Note: ASGI spec forbidden to edit the returned object. It's strictly
    read-only.

--- rollbar/contrib/starlette/__init__.py
+++ rollbar/contrib/starlette/__init__.py
@@ -1,9 +1,12 @@
-__all__ = ['StarletteMiddleware']
+__all__ = ['StarletteMiddleware', 'get_current_request']
 
 from starlette import __version__
 
 import rollbar
 from rollbar.contrib.starlette.middleware import StarletteMiddleware
+
+# Do not modify returned request object
+from rollbar.contrib.starlette.requests import get_current_request
 
 
 def _hook(request, data):
