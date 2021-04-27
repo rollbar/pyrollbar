9cd78fbe57ae0beec2dea0aa181ccac5df0c404d
Bart Skowron <bart.skowron@rollbar.com>
2021-04-27 22:25:38 +0200

    Lint code

--- rollbar/test/test_async.py
+++ rollbar/test/test_async.py
@@ -262,7 +262,9 @@ class AsyncLibTest(BaseTest):
         async_report_exc_info.assert_called_once()
 
     @mock.patch('rollbar.lib._async.report_exc_info')
-    def test_should_not_try_report_with_async_handler_if_non_async_handler(self, async_report_exc_info):
+    def test_should_not_try_report_with_async_handler_if_non_async_handler(
+        self, async_report_exc_info
+    ):
         import rollbar
         from rollbar.lib._async import RollbarAsyncError, try_report
         from rollbar.test.async_helper import run
