0cc3326d4d7aa47a0fc8689f7fa03728ba408566
Bart Skowron <bart.skowron@rollbar.com>
2021-05-06 15:50:36 +0200

    Raise the exception when incompatible middleware is trying to be used
    No need to raise exception while loading module

--- rollbar/contrib/starlette/logger.py
+++ rollbar/contrib/starlette/logger.py
@@ -3,19 +3,22 @@ __all__ = ['LoggerMiddleware']
 import logging
 import sys
 
-from starlette.types import Receive, Scope, Send
+from starlette.types import ASGIApp, Receive, Scope, Send
 
 from rollbar.contrib.asgi import ReporterMiddleware as ASGIReporterMiddleware
 from rollbar.contrib.starlette.requests import store_current_request
 
 log = logging.getLogger(__name__)
 
-if sys.version_info < (3, 7):
-    log.error('LoggerMiddleware requires Python 3.7')
-    raise RuntimeError('LoggerMiddleware requires Python 3.7')
-
 
 class LoggerMiddleware(ASGIReporterMiddleware):
+    def __init__(self, app: ASGIApp) -> None:
+        if sys.version_info < (3, 7):
+            log.error('LoggerMiddleware requires Python 3.7+')
+            raise RuntimeError('LoggerMiddleware requires Python 3.7+')
+
+        super().__init__(app)
+
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         store_current_request(scope, receive)
 
