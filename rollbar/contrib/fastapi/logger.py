ae2daeaf3b7df55759d26bd6fea0ba12ec0fa4a7
Bart Skowron <bart.skowron@rollbar.com>
2021-05-18 00:51:48 +0200

    Remove unused logger

--- rollbar/contrib/fastapi/logger.py
+++ rollbar/contrib/fastapi/logger.py
@@ -1,12 +1,9 @@
 __all__ = ['LoggerMiddleware']
 
-import logging
 from fastapi import __version__
 
 from rollbar.contrib.asgi.integration import integrate
 from rollbar.contrib.starlette import LoggerMiddleware as StarletteLoggerMiddleware
-
-log = logging.getLogger(__name__)
 
 
 @integrate(framework_name=f'fastapi {__version__}')
