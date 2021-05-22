9955666dcd9a711b91fd5b3a5762d56528b899de
Bart Skowron <bart.skowron@rollbar.com>
2021-05-22 18:44:51 +0200

    Add comment about optional requirements

--- rollbar/contrib/starlette/__init__.py
+++ rollbar/contrib/starlette/__init__.py
@@ -1,5 +1,11 @@
 __all__ = ['ReporterMiddleware', 'LoggerMiddleware', 'get_current_request']
 
+# Optional requirements:
+#
+# - Starlette requires `python-multipart` package to support requests body parsing
+# - `LoggerMiddleware` and `get_current_request` require `aiocontextvars` package
+#   to be installed when running in Python 3.6
+
 from .middleware import ReporterMiddleware
 from .logger import LoggerMiddleware
 
