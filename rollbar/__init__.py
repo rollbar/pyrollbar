8c67aaa6fdf2efa2d414344601ede232ebdf8349
Bart Skowron <bart.skowron@rollbar.com>
2021-05-31 16:40:58 +0200

    Bump version to 0.16.0

--- rollbar/__init__.py
+++ rollbar/__init__.py
@@ -23,7 +23,7 @@ import six
 from rollbar.lib import events, filters, dict_merge, parse_qs, text, transport, urljoin, iteritems, defaultJSONEncode
 
 
-__version__ = '0.15.2'
+__version__ = '0.16.0'
 __log_name__ = 'rollbar'
 log = logging.getLogger(__log_name__)
 
