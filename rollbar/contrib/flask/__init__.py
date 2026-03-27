bdf029c95c17061290c7dd19c991e6da33a600b5
Daniel Morell <daniel@cairntek.com>
2026-03-27 21:59:00 +0100

    Added new Flask init() function.

--- rollbar/contrib/flask/__init__.py
+++ rollbar/contrib/flask/__init__.py
@@ -2,8 +2,9 @@
 Integration with Flask
 """
 
-from flask import request
+from flask import request, got_request_exception, Flask
 import rollbar
+from rollbar.lib.session import reset_current_session, set_current_session
 
 
 def report_exception(app, exception):
@@ -17,3 +18,27 @@ def _hook(request, data):
         data['context'] = str(request.url_rule)
 
 rollbar.BASE_DATA_HOOK = _hook
+
+
+def init(app: Flask, access_token, environment='production', scrub_fields=None, url_fields=None, **kw):
+    """
+    Initializes the Rollbar Flask integration.
+    """
+    with app.app_context():
+        rollbar.init(
+            access_token=access_token,
+            environment=environment,
+            scrub_fields=scrub_fields,
+            url_fields=url_fields,
+            **kw,
+        )
+        # send exceptions from `app` to rollbar, using flask's signal system.
+        got_request_exception.connect(report_exception, app)
+
+    @app.before_request
+    def before_request():
+        set_current_session(dict(request.headers))
+
+    @app.teardown_request
+    def teardown_request(exception):
+        reset_current_session()
